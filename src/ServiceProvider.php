5ce63feff4f7fb9766ff6b1013419a50f1173f29
Gabriel Gomez <leandrogrotnig11@gmail.com>
2019-01-28 12:47:36 -0300

    Luba initialization

---
+++ src/ServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace GRG\Luba;
+
+use Illuminate\Support\ServiceProvider;
+
+class ServiceProvider extends ServiceProvider
+{
+    public function boot ()
+    {
+    }
+
+    public function register ()
+    {
+    }
+}
